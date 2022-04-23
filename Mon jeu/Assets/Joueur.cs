85a897bf3870801588c399e713cab9dd0069c82f
KayZen <pkayzen64@gmail.com>
2022-04-23 02:11:01 +0200

    Ajout des mouvements

--- Mon jeu/Assets/Joueur.cs
+++ Mon jeu/Assets/Joueur.cs
@@ -27,11 +27,15 @@ public class Joueur : MonoBehaviour {
 
     void Mouvement(Vector2 direction) {
         Debug.Log("Déplacement effectué" + direction);
+        RG2D.velocity = new Vector2(MoveSpeed * direction.x, RG2D.velocity.y);
     }
 
     void Saut() {
         Debug.Log("Saut effectué");
-        RG2D.velocity = new Vector2(RG2D.velocity.x, JumpForce);
+        if(!Jumping) {
+            RG2D.velocity = new Vector2(RG2D.velocity.x, JumpForce);
+            Jumping = true;
+        }
     }
 
     private void OnEnable() {
@@ -40,5 +44,24 @@ public class Joueur : MonoBehaviour {
 
     private void OnDisable() {
         controls.Disable();
+    }
+
+    void Update()
+    {
+
+        //     // Crounching
+        // if(MovY == -1){
+        //     transform.localScale = new Vector2(1f, 0.5f);
+        // }
+
+        // else{
+        //     transform.localScale = new Vector2(1f, 1f);
+        // }
+    
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Jumping = false;
     }
 }
